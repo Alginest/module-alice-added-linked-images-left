ff5365d6072d30db0c38498b1adec1cd25ca8a45
ado durak <76266634+Alginest@users.noreply.github.com>
2022-08-15 21:51:32 +0200

    Add files via upload

---
+++ src/data/webData.js
@@ -0,0 +1,94 @@
+import cryptoOne from "../img/projectImg/crypto1.png";
+import cryptoTwo from "../img/projectImg/crypto2.png";
+import cryptoThree from "../img/projectImg/crypto3.png";
+import cryptoFour from "../img/projectImg/crypto4.png";
+import movieOne from "../img/projectImg/movie1.png";
+import movieTwo from "../img/projectImg/movie2.png";
+import movieThree from "../img/projectImg/movie3.png";
+import movieFour from "../img/projectImg/movie4.png";
+import foodOne from "../img/projectImg/food1.png";
+import foodTwo from "../img/projectImg/food2.png";
+import foodThree from "../img/projectImg/food3.png";
+import foodFour from "../img/projectImg/food4.png";
+import webTwo from "../img/movie.png";
+import webThree from "../img/resto.png";
+import webFour from "../img/blog.png";
+import webFive from "../img/lava.png";
+import webSix from "../img/shop.png";
+import webSeven from "../img/town.png";
+import webEight from "../img/school.png";
+import webNine from "../img/kitchen.png";
+import webOne from "../img/webOne.png";
+
+export const webData = [
+  {
+    id: 0,
+    name: "Latos Crypto",
+    program: "React-JS / Mui",
+    images: [cryptoOne, cryptoTwo, cryptoThree, cryptoFour],
+    image: webOne,
+    about:
+      "Latos Crypto is a platform that let's u explore all about crpyto,and the market changes live with the coin gecko API.For all the crypto enthusiast and enjoyers of the crypto market.",
+    link: "https://latos-crypto.netlify.app/",
+    project: "https://github.com/Alginest/Latos-Crypto-Final",
+  },
+  {
+    id: 1,
+    name: "Latos Movies",
+    program: "React-JS / Mui",
+    image: webTwo,
+    images: [movieOne, movieTwo, movieThree, movieFour],
+    about:
+      "Latos Movie is a platform that let's u explore all movie's, it has the newest releases of all the upcoming movies and shows, using the moviedb API.",
+    link: "https://imoviess.netlify.app/",
+    project: "https://github.com/Alginest/IMovie-Project-Finished-",
+  },
+  {
+    id: 2,
+    name: "Latos Restourant",
+    program: "React-JS / Mui",
+    image: webThree,
+    images: [foodOne, foodTwo, foodThree, foodFour],
+    about:
+      "Latose is a website made for a small food service, it contains a amazing menu, with a menu planner that's using the spooncular API to get foods based on calories u wanna intake.",
+    link: "https://latosfood.netlify.app/",
+    project: "https://github.com/Alginest/spoonecular-meal-plan-latos",
+  },
+
+  {
+    id: 3,
+    name: "Latos Blog",
+    program: "React-JS / Mui",
+    image: webFour,
+  },
+  {
+    id: 4,
+    name: "Lava MObile",
+    program: "React-JS / Mui",
+    image: webFive,
+  },
+  {
+    id: 5,
+    name: "Zay Shop",
+    program: "React-JS / Mui",
+    image: webSix,
+  },
+  {
+    id: 6,
+    name: "The Town",
+    program: "React-JS / Mui",
+    image: webSeven,
+  },
+  {
+    id: 7,
+    name: "Grad School",
+    program: "React-JS / Mui",
+    image: webEight,
+  },
+  {
+    id: 8,
+    name: "Klassy Caffe",
+    program: "React-JS / Mui",
+    image: webNine,
+  },
+];
